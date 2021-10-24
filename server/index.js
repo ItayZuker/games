ee39fc70707a6727dc05f2efe65e8768c1c882ac
Panda-Itay <itay.z@pandats.com>
2021-10-24 09:06:22 +0300

    server serving files fix

--- server/index.js
+++ server/index.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const Random_Square_Box_Rout = require('./routs/random_square_box.rout.js')
 const app = express();
+const path = require('path')
 require('./db.js');
 
 app.set('json spaces', 2);
 app.use(express.json());
 
-
-
 app.use('/random-square-box', Random_Square_Box_Rout);
 
+app.use('/', express.static(path.join(__dirname, '../client/build')));
 
+app.use(express.static(__dirname));
 
-let port = 8080
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+let port = 8080;
 if(process.env.PORT) {
     port = process.env.PORT
-}
+};
 
 app.listen(port, () => {
-    console.log('server is running on port:' + port)
+    console.log('server is running on port:' + port);
 });
